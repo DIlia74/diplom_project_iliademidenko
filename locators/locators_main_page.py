3eec6b51019be6f5e46bf6b39f3065602f77ce62
co11apz <romanmalajev@gmail.com>
2024-11-28 21:33:51 +0300

    test

---
+++ locators/locators_main_page.py
@@ -0,0 +1,18 @@
+import os
+
+from page.base_page import WebPage
+from page.elements import WebElement
+from page.elements import ManyWebElements
+
+
+class MainPage(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv("MAIN_URL") or 'https://www.kpc.by/'
+
+        super().__init__(web_driver, url)
+
+    # btn_headers_domain = WebElement(xpath='//div[@id="menu-item"]//span[contains(text(),"Домены")]')
+    hdh = WebElement(xpath='/html/body/header/div/div[1]/div[1]/div[1]/a/img')
+
