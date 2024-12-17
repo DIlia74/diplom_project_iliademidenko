33e7a61a20a03be4eb10281402aacb37a98c7f08
DIlia74 <tiriugolok@gmail.com>
2024-12-17 21:54:23 +0300

    коммит 17.12.2024

--- app.py
+++ app.py
@@ -11,13 +11,123 @@ app = Flask(__name__)
 def welcome():
     """ Эта функция запуская и отвечает за процесс возврата результата welcome.html. """
 
-    return render_template('welcome.html')
+    return render_template('index.html')
 
 
 @app.route("/error")
 def error():
-    """Эта функция запуская и отвечает за процесс возврата результата test_error.html."""
-    return render_template('test_error.html')
+    """Эта функция запуская и отвечает за процесс возврата результата index.html."""
+    cmd = ["./scriptsh/error.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/ui_tests")
+def ui_tests():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/ui_tests.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/ui_test_header")
+def ui_test_header():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/ui_test_header.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/ui_test_footer")
+def ui_test_footer():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/ui_test_footer.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/ui_test_search")
+def ui_test_search():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/ui_test_search.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/api_tests")
+def api_test():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/api_tests.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/allure_ui")
+def allure_ui():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/allure_ui.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/allure_api")
+def allure_api():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/allure_api.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
+
+
+@app.route("/allure_summary")
+def allure_summary():
+    """ Эта функция запуская и отвечает за генерацию отчета allure. """
+
+    cmd = ["./scriptsh/allure_summary.sh"]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE,
+                          universal_newlines=True) as result:
+        out = result.communicate()
+    return render_template('index.html', text=out, json=out)
 
 
 @app.route("/runallure")
@@ -30,7 +140,7 @@ def run_allure():
                           stdin=subprocess.PIPE,
                           universal_newlines=True) as result:
         out = result.communicate()
-    return render_template('welcome.html', text=out, json=out)
+    return render_template('index.html', text=out, json=out)
 
 
 @app.route("/run_ui")
