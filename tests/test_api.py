2161efc1470c407da33152ecbde55c8e6e2baf61
DIlia74 <tiriugolok@gmail.com>
2024-12-18 21:49:03 +0300

    12-12-2024

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,17 +1,19 @@
 import requests
 
+
 def test_api():
-    url = 'https://auto.ru/'
+    url = 'https://www.kpc.by/'
     response = requests.get(url)
 
     print(response.status_code, 'status_code')
     print(response.headers, 'headers')
     print(response.cookies, 'cookies')
+    print(response.request, 'request')
 
 
 def test_api_get():
-    url = 'https://auto.ru/'
-    query = {'search_query': 'Haval'}
+    url = 'https://www.kpc.by/search'
+    query = {'search_query': 'принтер'}
     response = requests.get(url, params=query)
 
     print(response.headers, 'headers')
@@ -19,4 +21,3 @@ def test_api_get():
     print(response.url, 'url')
 
     print(response.json(), 'json')
-
